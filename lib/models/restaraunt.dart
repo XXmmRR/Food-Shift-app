e083b2a1b294de16e7f044acf09038959935f94b
XXmmRR <Xorex50@protonmail.com>
2024-03-05 20:23:29 +0500

    add Restaraunt model

---
+++ lib/models/restaraunt.dart
@@ -0,0 +1,11 @@
+class Restaraunt {
+  String name;
+  String waitTime;
+  String distance;
+  String label;
+  String logoUrl;
+  String desc;
+  num score;
+  Map<String, List<Food>> menu;
+  Restaraunt(this.name, this.waitTime, this.distance, this.label, this.logoUrl, this.desc, this.menu, this.score)
+}

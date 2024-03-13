436e6cb06142d7a9761bfee490e769dbf13349a1
XXmmRR <Xorex50@protonmail.com>
2024-03-14 00:21:19 +0500

    fixes

--- lib/screens/home/widget/food_item.dart
+++ lib/screens/home/widget/food_item.dart
@@ -12,37 +12,53 @@ class FoodItem extends StatelessWidget {
       height: 110,
       decoration: BoxDecoration(
         color: Colors.white,
-        borderRadius: BorderRadius.circular(20)
-        ),
-      child: Row(children: [
-        Container(
-          padding: EdgeInsets.all(5), 
-          width: 110,
-          height: 110,
-          child: Image.network(food.imgUrl, fit: BoxFit.fitHeight,),
+        borderRadius: BorderRadius.circular(20),
+      ),
+      child: Row(
+        children: [
+          Container(
+            padding: EdgeInsets.all(5),
+            width: 110,
+            height: 110,
+            child: Image.network(food.imgUrl, fit: BoxFit.fitHeight),
           ),
-          Expanded(child: Container(
-            padding: const EdgeInsets.only(top: 20, left: 10, right: 10),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [Text(food.name, style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold, height: 1.5) ,), 
-                  const Icon(Icons.arrow_forward_ios_outlined, size: 15,),
-                  Text(food.desc, style: TextStyle(color: food.highlight ? kPrimaryColor : Colors.grey.withOpacity(0.8), height: 1.5),),
-                  SizedBox(height: 5,),
+          Expanded(
+            child: Container(
+              padding: const EdgeInsets.only(top: 20, left: 10, right: 10),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                mainAxisAlignment: MainAxisAlignment.spaceBetween, // Распределение по вертикали
+                children: [
+                  Text(
+                    food.name,
+                    style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold, height: 1.5),
+                  ),
                   Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
                     children: [
-                      Text('${food.price} \$', style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),)
+                      Expanded(
+                        // Обеспечивает, что текст не выйдет за рамки экрана
+                        child: Text(
+                          food.desc,
+                          style: TextStyle(
+                              color: food.highlight ? kPrimaryColor : Colors.grey.withOpacity(0.8),
+                              height: 1.5),
+                          overflow: TextOverflow.ellipsis,
+                        ),
+                      ),
+                      const Icon(Icons.arrow_forward_ios_outlined, size: 15),
                     ],
-                    )
-                  ],
-                )
-              ],
+                  ),
+                  Text(
+                    '${food.price} \$',
+                    style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+                  ),
+                ],
+              ),
             ),
-          ))
-      ],)
+          ),
+        ],
+      ),
     );
   }
 }

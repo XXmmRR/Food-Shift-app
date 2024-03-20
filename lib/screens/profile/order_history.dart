008edaef783451f632ba10b4098da7da6eee1e77
XXmmRR <Xorex50@protonmail.com>
2024-03-20 15:24:55 +0500

    fix

--- lib/screens/profile/order_history.dart
+++ lib/screens/profile/order_history.dart
@@ -1,15 +1,33 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_application_2/widgets/custom_app_bar.dart';
+import 'package:flutter_application_2/widgets/custom_nav_bar.dart';
+import 'package:flutter_application_2/widgets/pages.dart';
 
-class OrderHistory extends StatelessWidget {
+class OrderHistory extends StatefulWidget {
   const OrderHistory({super.key});
 
   @override
+  State<OrderHistory> createState() => _OrderHistoryState();
+}
+
+class _OrderHistoryState extends State<OrderHistory> {
+
+  int pageindex = 0;
+        void onTabTapped(int index) {
+      print(pageindex);
+      setState(() {pageindex = index;});
+      Navigator.of(context).push(MaterialPageRoute(builder: (context) =>  pages[pageindex]));
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return Scaffold(body: Center(child: Column(
+    return Scaffold(
+      body: Center(child: Column(
         children: [
          CustomAppBar(leftIcon: Icons.arrow_back_ios_new, rightIcon: Icons.search_off_outlined),
-         Image.network('https://cdn4.iconfinder.com/data/icons/smooth-3d-for-online-stores/512/shopping-basket.png', fit: BoxFit.cover,),
-         Text('Order history not found')],)),);
+         Image.network('https://atlas-content-cdn.pixelsquid.com/stock-images/blue-box-5E80Re5-600.jpg', fit: BoxFit.cover,),
+         Text('Order history not found')],)),
+      bottomNavigationBar: CustomNavbar(onTabTapped),   
+         );
   }
 }

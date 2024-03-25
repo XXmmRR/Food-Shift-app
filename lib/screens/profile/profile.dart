d247ba11bd168ef75ce4536144228168215ab812
XXmmRR <Xorex50@protonmail.com>
2024-03-25 19:58:14 +0500

    add profile page and setting package

--- lib/screens/profile/profile.dart
+++ lib/screens/profile/profile.dart
@@ -1,10 +1,27 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_application_2/widgets/custom_nav_bar.dart';
+import 'package:flutter_application_2/widgets/pages.dart';
 
-class ProfilePage extends StatelessWidget {
+class ProfilePage extends StatefulWidget {
   const ProfilePage({super.key});
 
   @override
+  State<ProfilePage> createState() => _ProfilePageState();
+}
+
+class _ProfilePageState extends State<ProfilePage> {
+    int pageindex = 0;
+    void onTabTapped(int index) {
+      print(pageindex);
+      setState(() {pageindex = index;});
+      Navigator.of(context).push(MaterialPageRoute(builder: (context) =>  pages[pageindex]));
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+        appBar: AppBar(title: Text('Profile'),),
+        bottomNavigationBar: CustomNavbar(onTabTapped),   
+    );
   }
 }
